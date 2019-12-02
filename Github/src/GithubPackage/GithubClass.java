feaed2b52600bdb45aa164cb86a29f858cf1bf01
Gopi <Gopi@Gopi-PC>
2019-12-02 18:15:26 +0530

    remodified

--- Github/src/GithubPackage/GithubClass.java
+++ Github/src/GithubPackage/GithubClass.java
@@ -1,28 +1,41 @@
 package GithubPackage;
 
 public class GithubClass {
-	
+
 	private int id;
-	
+	private String sname;
 	private String name;
 	private String address;
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+
+	public String getSname() {
+		return sname;
+	}
+
 	public String getAddress() {
 		return address;
 	}
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
-public void setId(int id) {
-	this.id = id;
-}
 
-public int getId() {
-	return id;
-}
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getId() {
+		return id;
+	}
 }
